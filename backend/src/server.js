ad419b5ac954cb7083eb7d989957e2fdbc98df74
Brahmajyoti <brahmajyotinayak100@gmail.com>
2025-12-01 02:16:20 +0530

    Update CORS settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,26 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
-import interviewRoutes from "./routes/interviewRoutes.js"; 
+import interviewRoutes from "./routes/interviewRoutes.js";
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
 
-// Connect DB 
+app.use(cors({
+  origin: process.env.CLIENT_URL || "*", 
+  credentials: true, // 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+}));
+
+app.use(express.json());
+
+// Connect to Database
 connectDB(); 
 
+// Routes
 app.get("/", (req, res) => res.send("API is running"));
+app.use("/api/interviews", interviewRoutes);
 
-// ROUTES
-app.use("/api/interviews", interviewRoutes); 
 
 export default app;
