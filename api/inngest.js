6103a8a2b610aa50108a3df1badeacad2fed7c6f
Brahmajyoti <brahmajyotinayak100@gmail.com>
2025-12-01 03:11:21 +0530

    Migrate project structure to Vercel

---
+++ api/inngest.js
@@ -0,0 +1,15 @@
+import { Inngest } from "inngest";
+import { serve } from "inngest/node"; 
+import { functions } from "../backend/src/lib/functions.js";
+
+const inngest = new Inngest({ id: "interview_video" });
+
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+export default serve({
+  client: inngest,
+  functions: functions,
+});
