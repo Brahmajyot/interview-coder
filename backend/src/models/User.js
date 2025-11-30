1de745acd12c299a9f3083f3b6ce9722bd0a358c
Brahmajyoti <brahmajyotinayak100@gmail.com>
2025-11-30 21:57:49 +0530

    Fix Mongoose schema timestamps placement

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,31 +1,17 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-name:
-{
-    type:String,
-    required:true,
-},
-email:
-{
-    type:String,
-    required:true,
-    unique:true,
-},
-clerkId:
-{
-    type:String,
-    required:true,
-    unique:true,
-},
-profileImages:
-{
-    type:String,
-    default:"",
-},
-timestamps:true //createAt and UpdateAt
-
-})
-
-const User = mongoose.model("User",userSchema);
+const UserSchema = new mongoose.Schema(
+  {
+    
+    clerkId: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    name: { type: String },
+    profileImage: { type: String },
+  }, 
+  { 
+    
+    timestamps: true 
+  }
+);
+const User = mongoose.model("User",UserSchema);
 export default User;
