eab7e07e7075260ed9e6148cc20fe1ef46dd067d
Brahmajyoti <brahmajyotinayak100@gmail.com>
2025-11-30 11:12:22 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  PORT: process.env.PORT || 3000,
-  DB_URI: process.env.DB_URI || "default_db_uri",
+  PORT: process.env.PORT,
+  DB_URI: process.env.DB_URI,
+  NODE_ENV: process.env.NODE_ENV
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,30 @@
 import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
 const app = express();
 
 
+
+const __dirname = path.resolve();
+
 app.get("/", (req, res) => {
-  res.send("API is running... what");
+  res.send("what is your name");
 });
+app.get("/books",(req,res) => {
+  res.send("bhai hogaya")
+})
+// app ka deploy karnay ka tareeqa
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../fronted/dist")));
+
+  app.get("/{*any}", (req,res)=>{
+    res.sendFile(path.join(__dirname,"../fronted", "dist", "index.html"))
+  })
+}
+
+
+
+
 
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port: ${ENV.PORT}`);
